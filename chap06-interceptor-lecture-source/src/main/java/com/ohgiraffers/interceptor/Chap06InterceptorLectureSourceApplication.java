9d2ebdad4d0260d0036064c5e1330dcbd1447d8a
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-04-21 21:44:31 +0900

    chap06 rename

--- chap06-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/Chap06InterceptorLectureSourceApplication.java
+++ chap06-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/Chap06InterceptorLectureSourceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chao06InterceptorLectureSourceApplication {
+public class Chap06InterceptorLectureSourceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chao06InterceptorLectureSourceApplication.class, args);
+        SpringApplication.run(Chap06InterceptorLectureSourceApplication.class, args);
     }
 
 }
